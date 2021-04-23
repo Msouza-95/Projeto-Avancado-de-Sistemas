2ed1b1b527652e6983ccff55c5c480f9d3cbe7cf
Msouza <msdsantos96@gmail.com>
2021-04-22 22:19:15 -0300

    finished roteiro 6 parte 1

---
+++ src/main/java/roteiro6/parte1/Produto.java
@@ -0,0 +1,56 @@
+
+package roteiro6.parte1;
+
+/**
+ *
+ * @author MSOUZA
+ */
+public class Produto {
+    
+    private int id;
+    private String name;
+    private double preco;
+
+    public Produto(int id, String name , double preco){
+          this.id = id;
+          this.name = name;
+          this.preco = preco; 
+          
+    }
+    
+    @Override 
+    public String toString(){
+        return "id:"+ this.id + "\n" + "Nome:" + this.name +"\n" + "Preco:"+ this.preco + "\n"  ; 
+    }
+            
+    public int getId() {
+        return id;
+    }
+
+    
+    public void setId(int id) {
+        this.id = id;
+    }
+
+   
+    public String getName() {
+        return name;
+    }
+
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    
+    public double getPreco() {
+        return preco;
+    }
+
+    
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+    
+    
+}
